4d169ddbcb7cd1aab0f7140178d351b3afe6cd91
gaBriel nesTor bruFau <70489784+qukiiignb@users.noreply.github.com>
2023-07-01 17:48:09 -0300

    Create abs.py

---
+++ UNLOCK-CHALLENGES/1/a/abs.py
@@ -0,0 +1,8 @@
+num1 = -10
+num2 = 5.5
+
+absolute_value1 = abs(num1)
+absolute_value2 = abs(num2)
+
+print("Absolute value of", num1, "is", absolute_value1)
+print("Absolute value of", num2, "is", absolute_value2)
